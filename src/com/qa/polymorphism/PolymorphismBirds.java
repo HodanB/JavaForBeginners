18b62890983533b144ff9be6930171b887181d6f
HodanB <Hodanbarkhad@outlook.com>
2022-05-27 10:28:00 +0100

    push

---
+++ src/com/qa/polymorphism/PolymorphismBirds.java
@@ -0,0 +1,34 @@
+package com.qa.polymorphism;
+
+
+class Bird {
+	public void sing () {
+	System.out.println("Birds normall sing like this tweet tweet tweet");
+}
+}
+class Pigeon extends Bird{
+	public void sing () {
+		System.out.println("But pigeons sing like this Oi tweet you");
+	}
+}
+
+class Crow extends Bird { 
+	 public void sing() {
+		    System.out.println("Crows sing like this QAAA");
+}
+}
+
+ class PolymorphismBirds {
+	public static void main(String[] args) {
+		// Create a Animal object 
+		Bird myBird = new Bird();  
+		// Create a Pigeon object
+			   Bird myPigeon = new Pigeon(); 
+		// Create a Crow object
+			   Bird myCrow = new Crow();  
+		// Create a Dog object
+			    myBird.sing();
+			    myPigeon.sing();
+			    myCrow.sing();
+			  }
+	 }
