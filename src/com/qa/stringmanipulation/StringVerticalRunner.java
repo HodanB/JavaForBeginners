15ce43c6ecdf237bc1aa53c63bda1366b4caa7db
HodanB <Hodanbarkhad@outlook.com>
2022-05-29 14:52:06 +0100

    String

---
+++ src/com/qa/stringmanipulation/StringVerticalRunner.java
@@ -0,0 +1,14 @@
+package com.qa.stringmanipulation;
+
+
+
+public class StringVerticalRunner {
+
+	public static void main(String[] args) {
+		
+		StringManipulation solution = new StringManipulation();
+	     solution.printingVerticalLine("Hello World, it's me .");     
+	    }
+	
+
+}
---
+++ src/com/qa/stringmanipulation/VerticalReverseRunner.java
@@ -0,0 +1,18 @@
+package com.qa.stringmanipulation;
+
+
+
+public class VerticalReverseRunner {
+	
+	public static void main(String[] args) {
+		
+		StringManipulation solution = new StringManipulation();
+		solution.printingVerticalReverse("hello it is");
+	}
+
+
+
+
+	}
+
+
